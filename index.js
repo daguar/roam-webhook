2fe5ec92696b8dd2a559ee6e4b44eb303cc60d72
Dave Guarino <daguar@gmail.com>
2020-10-06 18:52:10 -0700

    Barebones alpha, still blocked on API reliability

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const RoamPrivateApi = require('roam-research-private-api');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || "8000";
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello dave");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  const body = req.body.Body;
+  console.log(body);
+  const roam = new RoamPrivateApi(process.env.ROAM_GRAPH_NAME, process.env.ROAM_EMAIL, process.env.ROAM_PASSWORD, {
+    headless: true,
+  });
+  roam.quickCapture( [ body ] );
+  res.status(200).send("success");
 });
 
 app.listen(port, () => {
