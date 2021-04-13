c6892d3b69bd012e27de7d9ebd4b37df30843166
Dave Guarino <daguar@gmail.com>
2021-04-12 18:32:16 -0700

    Updated to new package version, seems to fix previous issues

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || "8000";
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
-  const body = req.body.Body;
+  const body = req.body.myparam;
   console.log(body);
   const roam = new RoamPrivateApi(process.env.ROAM_GRAPH_NAME, process.env.ROAM_EMAIL, process.env.ROAM_PASSWORD, {
     headless: true,
   });
-  roam.quickCapture( [ body ] );
-  res.status(200).send("success");
+
+  const dailyNoteUid = roam.dailyNoteUid();
+
+  roam.logIn()
+      .then( () => roam.createBlock( body, dailyNoteUid ))
+      .then( result => roam.close() );
+  //roam.quickCapture( [ body ] );
+  //res.status(200).send("success");
 });
 
 app.listen(port, () => {
